60250df7b89e3025e75e1c8a283d0cc4ec168a6e
dsinghvi <dsinghvi@umich.edu>
2024-04-02 12:13:44 -0400

    (fix): add custom function and test

--- src/mixpeek/client.py
+++ src/mixpeek/client.py
@@ -2,16 +2,64 @@
 import typing
 import httpx
 
-from mixpeek.base_client import BaseMixpeek
+from .base_client import BaseMixpeek, \
+    AsyncBaseMixpeek
+from .types.message import Message
+from .types.model import Model
+from .types.settings import Settings
+from .types.generation_response import GenerationResponse
+from .core.request_options import RequestOptions
+
+
+# this is used as the default value for optional parameters
+OMIT = typing.cast(typing.Any, ...)
 
 
 class Mixpeek(BaseMixpeek): 
 
-    def index(self, image_filepath: str) -> None:
-        pass
+    def generate(
+        self,
+        *,
+        model: Model,
+        # Pydantic model that represents the JSON schema of the response
+        response_format: typing.Optional[typing.Any],
+        context: typing.Optional[str] = OMIT,
+        messages: typing.Sequence[Message],
+        settings: typing.Optional[Settings] = OMIT,
+        request_options: typing.Optional[RequestOptions] = None,
+    ) -> GenerationResponse:
+        if response_format is not None:
+            response_format = response_format.model_json_schema()
+        return self.generators.generate(
+            model=model,
+            response_format=response_format,
+            context=context,
+            messages=messages,
+            settings=settings,
+            request_options=request_options,
+        )
 
 
-class AsyncMixpeek(BaseMixpeek):
+class AsyncMixpeek(AsyncBaseMixpeek):
 
-    def index(self, image_filepath: str) -> None:
-        pass
+    async def generate(
+        self,
+        *,
+        model: Model,
+        # Pydantic model that represents the JSON schema of the response
+        response_format: typing.Optional[typing.Any],
+        context: typing.Optional[str] = OMIT,
+        messages: typing.Sequence[Message],
+        settings: typing.Optional[Settings] = OMIT,
+        request_options: typing.Optional[RequestOptions] = None,
+    ) -> GenerationResponse:
+        if response_format is not None:
+            response_format = response_format.model_json_schema()
+        return await self.generators.generate(
+            model=model,
+            response_format=response_format,
+            context=context,
+            messages=messages,
+            settings=settings,
+            request_options=request_options,
+        )
--- tests/custom/test_client.py
+++ tests/custom/test_client.py
@@ -1,6 +1,31 @@
 import pytest
+import mixpeek
+import typing
+import httpx
 
+from mixpeek.client import Mixpeek
+from pydantic import BaseModel
+
+
+class Company(BaseModel):
+    name: str
+
+
+class Companies(BaseModel):
+    company_details: typing.List[Company]
+
+# esteininger
 # Get started with writing tests with pytest at https://docs.pytest.org
-@pytest.mark.skip(reason="Unimplemented")
+@pytest.mark.skip(reason="This is an example test")
 def test_client() -> None:
-    assert True == True
+    client = Mixpeek(base_url="https://api.mixpeek.com", 
+                     api_key="<<YOUR_API_KEY>>",
+                     httpx_client=httpx.Client(follow_redirects=True))
+    response = client.generate(
+        model=mixpeek.Model(provider="GPT", model="gpt-3.5-turbo"),
+        response_format=Companies,
+        context="Please tell me the weather and make sure to respond in the provided JSON schema",
+        messages=[],
+        settings=mixpeek.Settings()
+    )
+    print(response)
