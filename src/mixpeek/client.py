d98176a21288effae6782fd78e153294f9e576c7
dsinghvi <dsinghvi@umich.edu>
2024-04-02 20:17:23 -0400

    (fix): add custom parse client

--- src/mixpeek/client.py
+++ src/mixpeek/client.py
@@ -1,6 +1,7 @@
 
 import typing
 import httpx
+import os
 
 from .base_client import BaseMixpeek, \
     AsyncBaseMixpeek
@@ -9,6 +10,8 @@ from .types.model import Model
 from .types.settings import Settings
 from .types.generation_response import GenerationResponse
 from .core.request_options import RequestOptions
+from .parse_client import MixpeekParseClient, \
+    AsyncMixpeekParseClient
 
 
 # this is used as the default value for optional parameters
@@ -16,6 +19,52 @@ OMIT = typing.cast(typing.Any, ...)
 
 
 class Mixpeek(BaseMixpeek): 
+
+    """
+    Use this class to access the different functions within the SDK. You can instantiate any number of clients with different configuration that will propogate to these functions.
+
+    Parameters:
+        - base_url: str. The base url to use for requests from the client.
+
+        - authorization: typing.Optional[str].
+
+        - index_id: typing.Optional[str].
+
+        - api_key: typing.Optional[typing.Union[str, typing.Callable[[], str]]].
+
+        - timeout: typing.Optional[float]. The timeout to be used, in seconds, for requests by default the timeout is 60 seconds, unless a custom httpx client is used, in which case a default is not set.
+
+        - httpx_client: typing.Optional[httpx.Client]. The httpx client to use for making requests, a preconfigured client is used by default, however this is useful should you want to pass in any custom httpx configuration.
+    ---
+    from mixpeek.client import Mixpeek
+
+    client = Mixpeek(
+        authorization="YOUR_AUTHORIZATION",
+        index_id="YOUR_INDEX_ID",
+        api_key="YOUR_API_KEY",
+        base_url="https://yourhost.com/path/to/api",
+    )
+    """
+
+    def __init__(
+        self,
+        *,
+        base_url: str,
+        authorization: typing.Optional[str] = None,
+        index_id: typing.Optional[str] = None,
+        api_key: typing.Optional[typing.Union[str, typing.Callable[[], str]]] = os.getenv("MIXPEEK_API_KEY"),
+        timeout: typing.Optional[float] = None,
+        httpx_client: typing.Optional[httpx.Client] = None
+    ):
+        super().__init__(
+            base_url=base_url,
+            authorization=authorization,
+            index_id=index_id,
+            api_key=api_key,
+            timeout=timeout,
+            httpx_client=httpx_client
+        )
+        self.parse = MixpeekParseClient(client_wrapper=self._client_wrapper)
 
     def generate(
         self,
@@ -41,6 +90,53 @@ class Mixpeek(BaseMixpeek):
 
 
 class AsyncMixpeek(AsyncBaseMixpeek):
+
+    """
+    Use this class to access the different functions within the SDK. You can instantiate any number of clients with different configuration that will propogate to these functions.
+
+    Parameters:
+        - base_url: str. The base url to use for requests from the client.
+
+        - authorization: typing.Optional[str].
+
+        - index_id: typing.Optional[str].
+
+        - api_key: typing.Optional[typing.Union[str, typing.Callable[[], str]]].
+
+        - timeout: typing.Optional[float]. The timeout to be used, in seconds, for requests by default the timeout is 60 seconds, unless a custom httpx client is used, in which case a default is not set.
+
+        - httpx_client: typing.Optional[httpx.AsyncClient]. The httpx client to use for making requests, a preconfigured client is used by default, however this is useful should you want to pass in any custom httpx configuration.
+    ---
+    from mixpeek.client import AsyncMixpeek
+
+    client = AsyncMixpeek(
+        authorization="YOUR_AUTHORIZATION",
+        index_id="YOUR_INDEX_ID",
+        api_key="YOUR_API_KEY",
+        base_url="https://yourhost.com/path/to/api",
+    )
+    """
+    def __init__(
+        self,
+        *,
+        base_url: str,
+        authorization: typing.Optional[str] = None,
+        index_id: typing.Optional[str] = None,
+        api_key: typing.Optional[typing.Union[str, typing.Callable[[], str]]] = os.getenv("MIXPEEK_API_KEY"),
+        timeout: typing.Optional[float] = None,
+        httpx_client: typing.Optional[httpx.AsyncClient] = None
+    ):
+        super().__init__(
+            base_url=base_url,
+            authorization=authorization,
+            index_id=index_id,
+            api_key=api_key,
+            timeout=timeout,
+            httpx_client=httpx_client
+        )
+        self.parse = AsyncMixpeekParseClient(
+            client_wrapper=self._client_wrapper
+        )
 
     async def generate(
         self,
