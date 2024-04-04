e46fba7796f61cee5c74ef1512c6639b087bb956
armandobelardo <armandoubelardo@gmail.com>
2024-04-04 08:30:27 -0400

    add a test to mirror demo gist

--- tests/custom/test_client.py
+++ tests/custom/test_client.py
@@ -1,31 +1,46 @@
 import pytest
 import mixpeek
-import typing
-import httpx
-
 from mixpeek.client import Mixpeek
 from pydantic import BaseModel
+import json
 
+class Authors(BaseModel):
+    author_email: str
 
-class Company(BaseModel):
-    name: str
+class PaperDetails(BaseModel):
+    paper_title: str
+    author: Authors
 
-
-class Companies(BaseModel):
-    company_details: typing.List[Company]
 
 # esteininger
 # Get started with writing tests with pytest at https://docs.pytest.org
 @pytest.mark.skip(reason="This is an example test")
 def test_client() -> None:
     client = Mixpeek(base_url="https://api.mixpeek.com", 
-                     api_key="<<YOUR_API_KEY>>",
-                     httpx_client=httpx.Client(follow_redirects=True))
-    response = client.generate(
-        model=mixpeek.Model(provider="GPT", model="gpt-3.5-turbo"),
-        response_format=Companies,
-        context="Please tell me the weather and make sure to respond in the provided JSON schema",
-        messages=[],
-        settings=mixpeek.Settings()
+                     api_key="<<YOUR_API_KEY>>")
+
+    file_output = client.extract(
+        file_url="https://mixpeek-public-demo.s3.us-east-2.amazonaws.com/parse/copy-protected.pdf"
     )
+
+    # file_output.output is of type Any, so you have to be careful how you manage it
+    corpus = json.dumps(file_output.output)
+
+    str_output = client.extract(
+        contents="Hello world"
+    )
+
+    print(str_output)
+
+    response = client.generate(
+        model={"provider": "GPT", "model": "gpt-3.5-turbo"},
+        response_format=PaperDetails,
+        context=f"format this document and make sure to respond and adhere to the provided JSON format: {corpus}",
+        messages=[],
+        settings={"temperature": 0.5},
+    )
+
     print(response)
+
+    embedding = client.embed(input="hello world")
+    print(embedding)
