cb04e704eb25163674b538a1dde575efc0da1b7a
Ethan Steininger <esteininger21@gmail.com>
2024-06-08 21:28:28 -0400

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='mixpeek',
-    version='0.6.23',
+    version='0.6.24',
     author='Ethan Steininger',
     author_email='ethan@mixpeek.com',
     description='Mixpeek Python SDK',
